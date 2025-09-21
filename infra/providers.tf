3b8cb3a98ede30144829d24b2e5fbc7796753123
Glauco Vilas <glaucovilas@Glaucos-MacBook-Air.local>
2025-09-21 17:27:41 -0300

    infra: update terraform version and rename volumes

--- infra/providers.tf
+++ infra/providers.tf
@@ -1,5 +1,7 @@
 # Providers used in this Terraform configuration.
 terraform {
+    required_version = "~> 1.13.0"
+    
     required_providers {
         # Provider Docker to manage Docker resources locally.
         docker = {
