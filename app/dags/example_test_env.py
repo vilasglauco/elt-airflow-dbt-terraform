8984eb611980bee46905eac3058ecc3e82dd212e
Glauco Vilas <vilasglauco@gmail.com>
2025-09-21 16:42:36 -0300

    feat: add extract DAG, dataset staging, and update example DAG

--- app/dags/example_test_env.py
+++ app/dags/example_test_env.py
@@ -1,22 +1,29 @@
-# This is a simple DAG to test the Airflow environment setup.
-# It contains a single task that shows "Hello World" in the logs. (PythonOperator)
+"""
+Example DAG to test the Airflow environment setup.
+This DAG demonstrates a simple task that prints "Hello World" to verify that the Airflow 
+environment is functioning correctly.
+"""
 
-# Imports may look unused or missing locally, but they are required when Airflow runs inside the container.
-from airflow import DAG
-from airflow.operators.python import PythonOperator
 from airflow.utils import timezone
+from airflow.decorators import dag, task
 
 
-def hello_world():
-    # A simple function that prints "Hello World".
-    print("Hello World from Airflow!")
-
-
-with DAG(
-    dag_id="dag_example_test_env",
+@dag(
+    dag_id="example_test_env",
     start_date=timezone.datetime(2025, 1, 1),
     schedule=None,  # Only execute on demand/manual trigger.
     catchup=False,
     tags=["test"],
-) as dag:
-    hello = PythonOperator(task_id="hello", python_callable=hello_world)
+)
+
+def example_test_env():
+    """ A simple DAG to test the Airflow environment setup. """
+    @task()
+    def hello_world():
+        # A simple function that prints "Hello World".
+        print("Hello World from Airflow!")
+
+    hello_world()
+
+
+dag = example_test_env()
